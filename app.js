69bbcc300ddb065e072a658e1e95b5ae6aeef7b6
Godwin Kibet <kibetgodwin13@gmail.com>
2025-02-18 20:28:36 +0300

    Code optimzation

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello Godwin");
+function total(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorect input!');
+    }
+    return n1 + n2;
+}
+var number3 = 15;
+var number4 = 20;
+var back = total(number3, number4);
+console.log(back);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("Hello Godwin");
+function total(n1:number, n2:number){
+    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+        throw new Error('Incorect input!');
+    }
+    return n1 + n2;
+}
+const number3 = 15;
+const number4 = 20;
+const back = total(number3, number4);
+console.log(back);
